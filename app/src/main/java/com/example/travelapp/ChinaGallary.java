6ae7586270f7757070d9604133ad0d3d344dab11
Viraj <paneliyaviraj100@gmail.com>
2023-04-19 00:22:01 -0400

    creates basic app structure

---
+++ app/src/main/java/com/example/travelapp/ChinaGallary.java
@@ -0,0 +1,15 @@
+package com.example.travelapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChinaGallary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_china_gallary);
+
+    }
+}
