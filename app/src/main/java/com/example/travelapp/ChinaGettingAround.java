466f97c9fb44500a763ff2f9b77b055ec6ecbf9c
Viraj <paneliyaviraj100@gmail.com>
2023-04-19 13:54:52 -0400

    xml changes

--- app/src/main/java/com/example/travelapp/ChinaGettingAround.java
+++ app/src/main/java/com/example/travelapp/ChinaGettingAround.java
@@ -2,21 +2,13 @@ package com.example.travelapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-
+public class ChinaGettingAround extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_china_getting_around);
-    }
-
-    public void openChinaHome(View view) {
-        Intent intent = new Intent(MainActivity.this, HomeChina.class);
-        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/travelapp/HomeFrance.java
+++ app/src/main/java/com/example/travelapp/HomeFrance.java
@@ -3,14 +3,12 @@ package com.example.travelapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class ChinaGallary extends AppCompatActivity {
+public class HomeFrance extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_china_gallary);
-
+        setContentView(R.layout.activity_home_france);
     }
 }
